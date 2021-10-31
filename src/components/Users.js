59a04ea3dfa025c552c4185a10fca7a4b337d5cc
Kristin <kristinponsonby@gmail.com>
2021-10-31 11:55:03 -0400

    Done.

--- src/components/Users.js
+++ src/components/Users.js
@@ -8,9 +8,9 @@ class Users extends Component {
         <ul>
           Users!
           {/* Write code here that displays the usernames of all users in the Redux store */}
-          {this.props.usernames}
+          {this.props.users}
+          {this.props.count}
           {/* In addition, display the total number of users curently in the store */}
-          {/* {this.props.usernames.length} */}
         </ul>
       </div>
     )
@@ -18,7 +18,8 @@ class Users extends Component {
 }
 
 const mapStateToProps = (state) => {
-  return { users: state.username };
+  return { users: state.username,
+  count: state.users.length };
 };
 
 export default connect(mapStateToProps)(Users);
